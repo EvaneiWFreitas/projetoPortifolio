a3c5fccc2aa7f00013005073134b2a11e3746cac
prwilliam <nwillian533@gmail.com>
2026-03-16 14:28:25 -0300

    Adding icons in React JS, React icons, and creating the Contact page....(Colocando icones no React JS, React icons e criando a página de Contatos)

--- portifolio/src/pages/Page404/index.jsx
+++ portifolio/src/pages/Page404/index.jsx
@@ -1,7 +1,7 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Container from "../../components/Container";
-import styles from "./Page4040.module.css";
+import styles from "./Page404.module.css";
 
 
 function Page404() {
