7e2a7f1284fd6b1dc517e47115da8a22ae307032
prwilliam <nwillian533@gmail.com>
2026-03-20 13:39:50 -0300

    Refactoring the code(Refatorando o código)

--- portifolio/src/pages/Home/index.jsx
+++ portifolio/src/pages/Home/index.jsx
@@ -1,37 +1,30 @@
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
 import { Link } from "react-router-dom";
 import Container from "../../components/Container";
 import styles from "./Home.module.css";
 
-
 function Home() {
   return (
     <>
-      <Header />
-      <Container>
-        <section className={styles.home}>
-          <div className={styles.apresentacao}>
-            <p>
-              Olá, sou <br />
-              <span>Evanei Freitas</span>
-              <br />
-              Dev Full Stack
-            </p>
-            <Link to="/sobre" className={`${styles.btn} ${styles.botao_red}`}>
-              Saiba mais sobre mim...
-            </Link>
-          </div>
-          <figure>
-            <img
-              className={styles.img_home}
-              src="/programador.svg"
-              alt="Imagem de home"
-            />
-          </figure>
-        </section>
-      </Container>
-      <Footer />
+      <section className={styles.home}>
+        {/*  */}
+        <div className={styles.apresentacao}>
+          <p>
+            Olá, sou <br />
+            <span>Evanei Freitas</span>
+            <br />
+            Dev Full Stack
+          </p>
+          <Link to="/sobre" className={`${styles.btn} ${styles.botao_red}`}>
+            Saiba mais sobre mim...
+          </Link>
+        </div>
+        <figure>
+          {/* */}
+          <img className={styles.img_home} src="/programador.svg"
+            alt="Imagem de home"
+          />
+        </figure>
+      </section>
     </>
   );
 }
